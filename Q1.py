a0ae1f6443246e98507e957e489f372a292a6e87
garadyusuf <93493037+garadyusuf@users.noreply.github.com>
2022-04-20 16:58:02 +0100

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,114 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# Create function fib that returns the n'th element of Fibonacci sequence
+
+def fib(n)
+SyntaxError: expected ':'
+def fib(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Invalid input")
+    else n = 0:
+        
+SyntaxError: expected ':'
+def fib(n)
+SyntaxError: expected ':'
+def fib(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Invalid input")
+        
+SyntaxError: expected ':'
+def fib(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Invalid input")
+    else n = 0:
+        return a
+    
+SyntaxError: expected ':'
+# Function for nth Fibonacci number
+ 
+def Fibonacci(n):
+    if n<= 0:
+        print("Incorrect input")
+    # First Fibonacci number is 0
+    elif n == 1:
+        return 0
+    # Second Fibonacci number is 1
+    elif n == 2:
+        return 1
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+ 
+# Driver Program
+ 
+print(Fibonacci(10))
+SyntaxError: invalid syntax
+
+=============================== RESTART: Shell ==============================
+# Fibonacci number
+
+def fib(n):
+    # First fib number is 0
+    elif n == 1:
+        
+SyntaxError: invalid syntax
+def fib(n):
+    elif n == 1:
+        
+SyntaxError: invalid syntax
+
+
+
+
+
+
+=============================== RESTART: Shell ==============================
+# Function for n'th fib number
+
+def fib(n):
+    # First number in the sequence is 0
+    elif n == 1:
+        
+SyntaxError: invalid syntax
+def fib(n):
+    # First fib number is 0
+    elif n = 1:
+        
+SyntaxError: invalid syntax
+def fib(n):
+    # First fib number is 0
+    elif n == 1:
+        
+SyntaxError: invalid syntax
+
+=============================== RESTART: Shell ==============================
+# Function for n'th fib number
+
+def fib(n):
+    # 1st number is 0
+    if n == 1:
+        print 0
+    # 2nd number is 1
+    elif n == 2:
+        print 1
+    else:
+        print fib(n-1)+fib(n-2)
+        
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+def fib(n):
+    
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+    
+print(fib(4))
+2
